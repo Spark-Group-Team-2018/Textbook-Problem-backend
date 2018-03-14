4176e644ffca1887a19b98dbeb0fa8b26e0a9add
Mechasparrow <mikipux7@gmail.com>
2018-03-13 23:02:52 -0500

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311181016) do
+ActiveRecord::Schema.define(version: 2018031110006) do
 
   create_table "books", force: :cascade do |t|
     t.string "ISBN"
