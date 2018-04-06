1a1234a799c4572ff6c5eec0df947c05c86c40b8
Mechasparrow <mikipux7@gmail.com>
2018-04-05 20:17:44 -0500

    added ability to register new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
-  skip_before_action :authenticate_request, only: [:index, :show]
+  skip_before_action :authenticate_request, only: [:index, :show, :create]
   before_action :set_user, only: [:show, :update, :destroy]
+
+
 
   def index
     @users = User.all()
@@ -12,8 +14,20 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def getuser
+    render json: @current_user
+  end
+
   ## TODO Add ability to register new user
   def create
+
+    @new_user = User.new(user_params)
+
+    if @new_user.save
+      render json: @new_user
+    else
+      render json: {"status": "unable to save new user"}
+    end
 
   end
 
@@ -27,8 +41,16 @@ class UsersController < ApplicationController
 
   private
 
+    def error_message
+      {"status": "unable to do specified action"}
+    end
+
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
